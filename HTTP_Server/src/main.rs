e5040fa282233adfa1949661d1d39f2d0186a666
dhruv <dhruvjain@gmail.com>
2024-11-25 01:44:25 +0530

    I have added an html page which is used to host the website instead of CLI. you can only start the server and not stop it. To stop it you will have to manually stop the server by pressing ctrl + c in CLI.

--- HTTP_Server/src/main.rs
+++ HTTP_Server/src/main.rs
@@ -1,18 +1,51 @@
+use actix_cors::Cors;
+use actix_web::{web, App, HttpServer, Responder};
+use serde::Serialize;
+use serde::Deserialize;
 use std::net::SocketAddr;
 
-#[tokio::main]
-async fn main() {
-    // Define the directory to serve static files from
-    let static_files = warp::fs::dir("./public");
+#[derive(Serialize)]
+struct JsonResponse {
+    message: String,
+}
 
-    // Serve static files on the root route
-    let routes = static_files;
+#[derive(Deserialize, Debug)]
+struct PostRequestData {
+    url: String,
+}
 
-    // Define the server address and port
-    let addr: SocketAddr = ([127, 0, 0, 1], 8080).into();
+async fn handle_request(data: web::Json<PostRequestData>) -> impl Responder {
+    // Print the request details to the console
+    println!("Received request: {:?}", data.url);
 
-    println!("Server running at http://{}/", addr);
+    let static_files = warp::fs::dir(data.url.to_string());
 
-    // Start the warp server
-    warp::serve(routes).run(addr).await;
+    // let routes = static_files;
+
+    let addr: SocketAddr = ([127, 0, 0, 2], 1234).into();
+
+    warp::serve(static_files).run(addr).await;
+
+    
+
+    // Respond with a JSON object
+    web::Json(JsonResponse {
+        message: "Server running at http://127.0.0.2:1234/".to_string(),
+    })
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    let server_address = "127.0.0.1:8080";
+    println!("Server running on http://{}", server_address);
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Cors::permissive())
+            .route("/api/endpoint", web::post().to(handle_request)) // Define the endpoint
+    })
+    .bind(server_address)?
+    .run()
+    .await
 }
